d602191ac78eb130439a428c593b0eff341b64d6
XTT <2308748900@qq.com>
2023-03-14 23:19:15 +0800

    first commit

---
+++ src/test/java/com/git_study2/gitstudy/GitstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git_study2.gitstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
